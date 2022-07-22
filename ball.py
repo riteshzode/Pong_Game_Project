ae3526ab3c5248cb645d7f9c1c9f85220b35da93
Ritesh Zode <riteshzode1122@gmail.com>
2022-07-22 20:45:19 +0530

    project udated

--- ball.py
+++ ball.py
@@ -14,21 +14,25 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
 
+
     # This move function will increase x and y coordinate by 10 as it will run
     def move(self):
         x = self.xcor() + self.xmove
         y = self.ycor() + self.ymove
         self.goto(x, y)
 
+
     # this both x and y reverse the value +ve to -ve or -ve to +ve
     def y_reverse(self):
         self.ymove *= -1
         # self.ymove = self.ymove * -1
 
+
     def x_reverse(self):
         self.xmove *= -1
         self.move_speed *= 0.9
         # self.xmove = self.xmove * -1
+
 
     # this function will reset tha ball to center
     def ball_reset(self):
